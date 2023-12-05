a8e19d44a77b5a42c40e1cb8ee084b0038940dd9
known <mondo.beyondo3@gmail.com>
2023-12-05 18:07:44 -0500

    Test integrating C lib functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::ffi::c_char;
+
+extern "C" {
+    fn set_raw_term();
+    fn set_cooked_term();
+    fn get_ch() -> c_char;
+}
+
 fn main() {
     println!("Hello, world!");
+    unsafe {
+        set_raw_term();
+    }
+    unsafe {
+        println!("{}", get_ch() as u8 as char);
+    }
+    unsafe {
+        set_cooked_term();
+    }
 }
