a30aa74dc404607139ad6e170292e94d2a798820
known <mondo.beyondo3@gmail.com>
2023-12-05 17:52:02 -0500

    Initial commit, contents from 'The Cargo Book'

---
+++ build.rs
@@ -0,0 +1,22 @@
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let out_dir = "src/clib/";
+
+    Command::new("gcc")
+        .args(&["src/termc.c", "-c", "fPIC", "-o"])
+        .arg(&format!("{}termc.o", out_dir))
+        .status()
+        .unwrap();
+
+    Command::new("ar")
+        .args(&["crus", "libtermc.a", "termc.o"])
+        .current_dir(&Path::new(out_dir))
+        .status()
+        .unwrap();
+
+    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-lib=static=termc");
+    println!("cargo:rerun-if-changed=src/termc.c");
+}
