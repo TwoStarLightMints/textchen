26440c4e882e0a2aae2d1b8ddf94062d3ff6c84c
known <mondo.beyondo3@gmail.com>
2023-12-08 20:20:25 -0500

    Implement necessary methods to be able to interact properly with gap buffer

--- src/gapbuf.rs
+++ src/gapbuf.rs
@@ -1,1 +1,49 @@
-pub struct GapBuf {}
+use std::fmt;
+
+pub struct GapBuf {
+    lhs: Vec<char>,
+    rhs: Vec<char>,
+}
+
+impl GapBuf {
+    pub fn new() -> Self {
+        Self {
+            lhs: Vec::new(),
+            rhs: Vec::new(),
+        }
+    }
+
+    // Here, ind refers to the index at which to split the source string
+    pub fn from_str(mut src: String, ind: usize) -> Self {
+        Self {
+            rhs: src.split_off(ind).chars().collect(),
+            lhs: src.chars().collect(),
+        }
+    }
+
+    pub fn insert(&mut self, c: char) {
+        self.lhs.push(c);
+    }
+
+    pub fn pop(&mut self) {
+        self.lhs.pop();
+    }
+
+    pub fn len(&self) -> usize {
+        self.lhs.iter().chain(self.rhs.iter()).count()
+    }
+
+    pub fn collect_to_string(self) -> String {
+        self.lhs.into_iter().chain(self.rhs.into_iter()).collect()
+    }
+}
+
+impl fmt::Display for GapBuf {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.lhs.iter().chain(self.rhs.iter()).collect::<String>()
+        )
+    }
+}
