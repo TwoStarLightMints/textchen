0b03c803cd613ef6df93a61fa802bee7faf9230a
known <mondo.beyondo3@gmail.com>
2023-12-09 01:17:02 -0500

    Add helper functions to encapsulate some methods of moving the cursor

--- src/cursor.rs
+++ src/cursor.rs
@@ -3,11 +3,24 @@ use crate::term::move_cursor_to;
 pub struct Cursor {
     pub row: u32,
     pub column: u32,
+    pub prev_row: u32,
+    pub prev_col: u32,
 }
 
 impl Cursor {
     pub fn new(row: u32, column: u32) -> Self {
-        Self { row, column }
+        Self {
+            row,
+            column,
+            prev_row: 0, // Both initialized to 0 so there's no need for options
+            prev_col: 0,
+        }
+    }
+
+    pub fn move_to(&mut self, new_row: u32, new_col: u32) {
+        self.row = new_row;
+        self.column = new_col;
+        self.update_pos()
     }
 
     pub fn move_up(&mut self) {
@@ -34,5 +47,16 @@ impl Cursor {
 
     pub fn update_pos(&self) {
         move_cursor_to(self.column, self.row)
+    }
+
+    pub fn save_current_pos(&mut self) {
+        self.prev_row = self.row;
+        self.prev_col = self.column;
+    }
+
+    pub fn revert_pos(&mut self) {
+        self.row = self.prev_row;
+        self.column = self.prev_col;
+        move_cursor_to(self.column, self.row);
     }
 }
