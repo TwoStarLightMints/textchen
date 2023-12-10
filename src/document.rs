a92ca5837670d3abe5c79e08eaafc642865b0962
known <mondo.beyondo3@gmail.com>
2023-12-09 22:55:17 -0500

    Fully implement Line struct, convert methods to be appropriate to modify Lines in document

--- src/document.rs
+++ src/document.rs
@@ -1,6 +1,18 @@
 use std::fmt::Display;
 
-struct Line(Vec<usize>, String);
+pub struct Line(pub Vec<usize>, pub String);
+
+impl Line {
+    pub fn new() -> Self {
+        Self(Vec::new(), "".to_string())
+    }
+
+    pub fn from_str(src: String) -> Self {
+        // src is assumed to be a single line string, not containing any new line characters, creation of multiple Lines is to be done outside of this function
+        // so will to the insertion of indices
+        Self(Vec::new(), src)
+    }
+}
 
 pub struct Document {
     pub file_name: String,
@@ -13,7 +25,7 @@ impl Document {
         let mut lines: Vec<Line> = Vec::new();
 
         for line in content.lines() {
-            let mut new_line = Line(Vec::new(), line.to_string());
+            let mut new_line = Line::from_str(line.to_string());
 
             if line.len() <= width as usize {
                 new_line.0 = vec![curr_ind];
@@ -21,33 +33,55 @@ impl Document {
                 curr_ind += 1;
             } else {
                 let overflow = line.len() / width as usize;
-                let mut inds: Vec<_> = Vec::new();
 
-                for i in 0..overflow {
-                    inds.push(curr_ind + i);
+                for i in 0..=overflow {
+                    new_line.0.push(curr_ind + i);
                 }
 
                 curr_ind += overflow;
             }
+
+            lines.push(new_line);
         }
 
         Self { file_name, lines }
     }
 
-    pub fn get_line_from_cursor_pos(&self, cursor_row: u32) -> String {
-        for line in self.lines {
-            if line.0.contains(cursor_row) {
-                line.1.clone()
+    pub fn get_line_at_cursor(&self, cursor_row: u32) -> String {
+        for line in self.lines.iter() {
+            if line.0.contains(&((cursor_row - 2) as usize)) {
+                return line.1.clone();
             }
         }
-        self.lines[(cursor_row - 2) as usize].clone()
+
+        panic!("Not found");
     }
 
-    pub fn set_line_from_cursor_pos(&mut self, cursor_row: u32, new_line: String) {
-        self.lines[(cursor_row - 2) as usize] = new_line;
+    pub fn set_line_at_cursor(&mut self, cursor_row: u32, new_line: String) {
+        let mut changed_line = Line::from_str(new_line);
+        changed_line.0 = self.lines[(cursor_row - 2) as usize].0.clone();
+        self.lines[(cursor_row - 2) as usize] = changed_line;
     }
 
     pub fn to_string(&self) -> String {
-        self.lines.join("\n")
+        self.lines
+            .iter()
+            .map(|e| e.1.clone())
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+}
+
+impl Display for Document {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.lines
+                .iter()
+                .map(|e| e.1.clone())
+                .collect::<Vec<String>>()
+                .join("\n")
+        )
     }
 }
