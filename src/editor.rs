dfc65d1306ecb44690307ae4ffa5d713b810a404
known <mondo.beyondo3@gmail.com>
2023-12-17 01:08:00 -0500

    Move all editor specifc functions to this file

---
+++ src/editor.rs
@@ -0,0 +1,180 @@
+use crate::cursor::*;
+use crate::document::*;
+use crate::term::print_flush;
+use std::io::{self, Write};
+
+#[derive(PartialEq, Eq)]
+pub enum Modes {
+    Normal,
+    Insert,
+    Command,
+    MoveTo,
+}
+
+pub fn display_line(
+    editor_left_edge: usize,
+    editor_width: usize,
+    document: &Document,
+    cursor: &mut Cursor,
+) {
+    let curr_row = cursor.row;
+    let curr_col = cursor.column;
+
+    let line = document.get_line_at_cursor(cursor.row);
+
+    cursor.move_to_start_line(document, editor_left_edge);
+
+    for (ind, char) in line.1.chars().enumerate() {
+        print_flush(format!("{char}").as_str());
+
+        if ind != 0 && (ind + 1) % editor_width == 0 && ind != line.1.len() - 1 {
+            cursor.move_down();
+            cursor.move_to_editor_left(editor_left_edge);
+        }
+    }
+
+    cursor.move_to(curr_row, curr_col);
+}
+
+pub fn display_document(
+    document: &Document,
+    editor_left_edge: usize,
+    editor_width: usize,
+    cursor: &mut Cursor,
+) {
+    //! document - Document being edited
+    //! editor_left_edge - This is the offset from the left side of the terminal
+    //! editor_width - Size of the editor screen, calculated from the left side offset and the right side offset, pass this calculated result to the function
+    //! cursor - Get control of cursor
+    //!
+    //! Displays the document that is currently being edited to the screen, handles drawing within given bounds
+
+    // This is used here instead of save position as it messes with something outside of this scope
+    let curr_row = cursor.row;
+    let curr_col = cursor.column;
+
+    cursor.move_to(2, editor_left_edge);
+
+    for line in document.lines.iter() {
+        for (ind, char) in line.1.chars().enumerate() {
+            print_flush(format!("{char}").as_str());
+
+            if ind != 0 && (ind + 1) % editor_width == 0 && ind != line.1.len() - 1 {
+                cursor.move_down();
+                cursor.move_to_editor_left(editor_left_edge);
+            }
+        }
+
+        cursor.move_down();
+        cursor.move_to_editor_left(editor_left_edge);
+    }
+
+    cursor.move_to(curr_row, curr_col);
+}
+
+pub fn clear_line(
+    editor_left_edge: usize,
+    editor_width: usize,
+    document: &Document,
+    cursor: &mut Cursor,
+) {
+    let curr_row = cursor.row;
+    let curr_col = cursor.column;
+
+    cursor.move_to_start_line(document, editor_left_edge);
+
+    for _ in document.get_line_at_cursor(cursor.row).0 {
+        print!("{: <1$}", "", editor_width);
+    }
+
+    cursor.move_to(curr_row, curr_col);
+}
+
+pub fn clear_editor_window(editor_right_edge: usize, document: &Document, cursor: &mut Cursor) {
+    //! editor_right_edge - This is the offset from the right side of the terminal
+    //! document - Document being edited
+    //! cursor - Get control of cursor
+    //!
+    //! Visually clears the contents of the editor window, the rest of the screen is untouched
+
+    // This is used here instead of save position as it messes with something outside of this scope
+    let curr_row = cursor.row;
+    let curr_col = cursor.column;
+
+    cursor.move_to(2, 1);
+
+    for _ in 0..=document.num_rows() {
+        // print!("\u{001b}[2K");
+        print!("{: >1$}", "", editor_right_edge);
+        cursor.move_down();
+    }
+
+    cursor.move_to(curr_row, curr_col);
+}
+
+pub fn reset_line_view(
+    editor_left_edge: usize,
+    editor_width: usize,
+    document: &Document,
+    cursor: &mut Cursor,
+) {
+    let curr_row = cursor.row;
+    let curr_col = cursor.column;
+
+    clear_line(editor_left_edge, editor_width, document, cursor);
+
+    display_line(editor_left_edge, editor_width, document, cursor);
+
+    cursor.move_to(curr_row, curr_col);
+}
+
+pub fn reset_editor_view(
+    document: &Document,
+    editor_left_edge: usize,
+    editor_right_edge: usize,
+    cursor: &mut Cursor,
+) {
+    //! editor_right_edge - This is the offset from the right side of the terminal
+    //! editor_left_edge - This is the offset from the left side of the terminal
+    //! document - Document being edited
+    //! cursor - Get control of cursor
+    //!
+    //! Clears the editor screen and redraws the document provided, tends to be used as to refresh the screen after an edit has occurred
+
+    clear_editor_window(editor_right_edge, document, cursor);
+
+    display_document(
+        document,
+        editor_left_edge,
+        editor_right_edge - editor_left_edge,
+        cursor,
+    );
+}
+
+pub fn change_mode(curr: &mut Modes, new_mode: Modes, mode_row: usize, cursor: &mut Cursor) {
+    //! curr - Current mode stored in the state of the application
+    //! new_mode - The new mode which will be stored in the state of the application
+    //! mode_row - The row at which the mode will be printed
+    //! cursor - Get control of cursor
+    //!
+    //! Changes the current mode of the editor to a new target mode, handles changing state and drawing to screen
+
+    *curr = new_mode;
+
+    // This is used here instead of save position as it messes with something outside of this scope
+    let curr_row = cursor.row;
+    let curr_col = cursor.column;
+
+    cursor.move_to(mode_row, 0);
+
+    match curr {
+        Modes::Normal => print!("NOR"),
+        Modes::Insert => print!("INS"),
+        Modes::Command => print!("COM"),
+        Modes::MoveTo => print!("MOV"),
+    };
+
+    io::stdout().flush().unwrap();
+
+    cursor.move_to(curr_row, curr_col);
+}
