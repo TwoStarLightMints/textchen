c8a241f1d1bae1c10043ffecd4ce618e2c39ef78
known <mondo.beyondo3@gmail.com>
2023-12-17 01:07:46 -0500

    Move all debugging functions to this file

---
+++ src/debug.rs
@@ -0,0 +1,63 @@
+use crate::cursor::Cursor;
+use crate::document::Document;
+use crate::gapbuf::GapBuf;
+use crate::term::Wh;
+use std::fs::File;
+use std::io::Write;
+
+#[derive(PartialEq, Eq)]
+pub enum Modes {
+    Normal,
+    Insert,
+    Command,
+    MoveTo,
+}
+
+#[allow(dead_code)]
+pub fn debug_log_message(message: String, log_file: &mut File) {
+    log_file.write(message.as_bytes()).unwrap();
+}
+
+#[allow(dead_code)]
+pub fn debug_log_document(document: &Document, log_file: &mut File) {
+    document.lines.iter().for_each(|l| {
+        log_file
+            .write(format!("Line indices: {:?}, String content: {}\n", l.0, l.1).as_bytes())
+            .unwrap();
+    })
+}
+
+#[allow(dead_code)]
+pub fn debug_log_dimensions(dimensions: &Wh, log_file: &mut File) {
+    log_file
+        .write(
+            format!(
+                "Terminal width: {}, Terminal height: {}\n",
+                dimensions.width, dimensions.height
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+}
+
+#[allow(dead_code)]
+pub fn debug_log_cursor(cursor: &Cursor, log_file: &mut File) {
+    log_file
+        .write(
+            format!(
+                "Cursor row: {}, Cursor row relative to document: {}, Cursor column: {}\n",
+                cursor.row,
+                cursor.row - 2,
+                cursor.column
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+}
+
+#[allow(dead_code)]
+pub fn debug_log_gapbuffer(gap_buf: &GapBuf, log_file: &mut File) {
+    log_file
+        .write(format!("Lhs: {:?}, Rhs: {:?}\n", gap_buf.lhs, gap_buf.rhs).as_bytes())
+        .unwrap();
+}
