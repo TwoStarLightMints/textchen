7cae09d1ca58b68633f8af4165714081846b538f
known <mondo.beyondo3@gmail.com>
2023-12-19 14:54:56 -0500

    Add a create_dir call to make the clib directory in src if it does not exist

--- build.rs
+++ build.rs
@@ -1,8 +1,14 @@
+use std::fs::create_dir;
 use std::path::Path;
 use std::process::Command;
 
 fn main() {
     let out_dir = "src/clib/";
+
+    match create_dir(out_dir) {
+        Ok(_) => println!("Creating C library directory at {out_dir}..."),
+        Err(_) => println!("C library directory already exists, skipping..."),
+    }
 
     Command::new("gcc")
         .args(&["src/termc.c", "-c", "-fPIC", "-o"])
